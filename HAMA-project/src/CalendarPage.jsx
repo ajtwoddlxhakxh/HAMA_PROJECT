03dd919d6a9eb73ae4a9613cc623c14401f3f0d5
osori <a01024784424@gmail.com>
2025-10-31 01:24:07 +0900

    2차ㅏ 수정

---
+++ HAMA-project/src/CalendarPage.jsx
@@ -0,0 +1,7 @@
+export default function CalendarPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>캘린더</h1>
+    </div>
+  );
+}
---
+++ HAMA-project/src/ChatPage.jsx
@@ -0,0 +1,7 @@
+export default function ChatPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>채팅방</h1>
+    </div>
+  );
+}
